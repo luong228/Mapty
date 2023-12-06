a80d2e18372323d7389c34726768d3431d0c201b
luong228 <90311663+luong228@users.noreply.github.com>
2023-12-06 16:20:43 +0700

    Load current location on Leaflet

--- script.js
+++ script.js
@@ -14,9 +14,19 @@ const inputElevation = document.querySelector('.form__input--elevation');
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
     position => {
-     const {latitude} = position.coords
-     const {longitude} = position.coords    
-     console.log(`https://google.com/maps/@${latitude},${longitude}`);
+     const {latitude} = position.coords;
+     const {longitude} = position.coords;  
+     const coords = [latitude, longitude];
+
+     const map = L.map('map').setView(coords, 13);
+
+L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker(coords).addTo(map)
+    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+    .openPopup();
     },
     () => {
       alert('Could not get your position');
